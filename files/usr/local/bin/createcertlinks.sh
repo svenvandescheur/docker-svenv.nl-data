50654556770f93dc0fba1456a3d7bffe25521faf
Sven van de Scheur <svenvandescheur@gmail.com>
2017-01-08 17:27:02 +0100

    Different approach

--- files/usr/local/bin/createcertlinks.sh
+++ files/usr/local/bin/createcertlinks.sh
@@ -8,8 +8,12 @@
 
 
 # Link certificates
-dir=`ls -t /etc/letsencrypt/live/ | head -n 1`
+hostname=`hostname`
+dir=`ls -t /etc/letsencrypt/live/ | grep $hostname | head -n 1`
 path="/etc/letsencrypt/live/$dir"
+
+rm -f /etc/ssl/certs/svenv.nl.crt
+rm -f /etc/ssl/certs/svenv.nl.key
 
 ln -s "$path/privkey.pem" "/etc/ssl/certs/svenv.nl.key"
 ln -s "$path/fullchain.pem" "/etc/ssl/certs/svenv.nl.crt"
